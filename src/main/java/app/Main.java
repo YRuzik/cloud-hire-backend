1c17abf31ca160aa49cf4ab5caa3f183f3fcf9db
sad1ant <ilya.savelev.2023@gmail.com>
2024-03-11 03:08:43 +0300

    updated config and main files

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,6 +4,7 @@ import app.config.PersistenceConfig;
 import com.sun.net.httpserver.HttpServer;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
@@ -12,14 +13,20 @@ public class Main {
         EntityManagerFactory emf = PersistenceConfig.getEntityManagerFactory();
         EntityManager em = emf.createEntityManager();
 
+        initializeDatabase(em);
+        startServer();
+
+        System.out.println("Server started in port 8000");
+    }
+
+    private static void initializeDatabase(EntityManager em) {
         em.getTransaction().begin();
         em.getTransaction().commit();
-
         em.close();
-        emf.close();
+    }
 
+    private static void startServer() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8000), 0);
         server.start();
-        System.out.println("Server started in port 8000");
     }
 }
--- src/main/java/app/config/PersistenceConfig.java
+++ src/main/java/app/config/PersistenceConfig.java
@@ -12,11 +12,13 @@ public class PersistenceConfig {
         String dbUrl = dotenv.get("DATABASE_URL");
         String dbUsername = dotenv.get("DATABASE_USERNAME");
         String dbPassword = dotenv.get("DATABASE_PASSWORD");
+        String dbDriver = dotenv.get("DATABASE_DRIVER");
 
         Map<String, String> properties = new HashMap<>();
         properties.put("jakarta.persistence.jdbc.url", dbUrl);
         properties.put("jakarta.persistence.jdbc.user", dbUsername);
         properties.put("jakarta.persistence.jdbc.password", dbPassword);
+        properties.put("jakarta.persistence.jdbc.driver", dbDriver);
 
         return Persistence.createEntityManagerFactory("CloudHire", properties);
     }
